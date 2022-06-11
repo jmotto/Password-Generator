fb8bef8a39340080ef15ec1657b36a0242f33f43
Jamie Otto <jmotto31@gmail.com>
2022-06-10 20:10:13 -0700

    named characters in variables

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+// What sort of data does my application need to work?
+
+// Storing all needed characters in variables.
+
+// numbers
+
+var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+// special characters
+
+var specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+
+// uppercase letters
+
+var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+
+// lower case letters
+
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+// What sorts of tasks does my code need to complete?
+
+// Generate password
+
+function generatePassword() {
+
+  // prompt user for their password length and store it in a variable
+
+  // validate the password length and make sure its a number between or equal to 8 and 128
+
+  // prompt the user for if they want special characters and store in variable
+
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
+}
+
+function generateBtn() {
+
+}
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
